5afc06e1da386ca14d802822f2d0246b5f451853
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-13 21:25:22 +0800

    Added home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
 
 export default function Home() {
   return (
     <div>
+      <Announcement />
       <Navbar />
       Homepage
     </div>
