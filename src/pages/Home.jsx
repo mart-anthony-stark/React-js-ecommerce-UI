c06be58f262f776915275eee41cef451c43f6808
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-14 19:28:39 +0800

    Added categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement";
 import Slider from "../components/Slider";
+import Categories from '../components/Categories'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Announcement />
       <Navbar />
       <Slider />
+      <Categories />
     </div>
   );
 }
