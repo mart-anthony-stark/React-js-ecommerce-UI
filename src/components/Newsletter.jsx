9e27383ad5eacab53e1a85fe9cdf413d13583dd7
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-14 21:30:17 +0800

    Added newsletter component

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,61 @@
+import { Send } from "@material-ui/icons";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60vh;
+  background: #fcfcf5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+  margin-bottom: 20px;
+`;
+
+const Description = styled.div`
+  font-size: 24px;
+  font-weight: 300;
+  margin-bottom: 20px;
+`;
+
+const InputContainer = styled.div`
+  width: 50%;
+  height: 40px;
+  background: white;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid lightgrey;
+`;
+
+const Input = styled.input`
+  border: none;
+  flex: 8;
+  padding-left: 20px;
+`;
+
+const Button = styled.button`
+  flex: 1;
+  border: none;
+  background: teal;
+  color: white;
+`;
+
+const Newsletter = () => {
+  return (
+    <Container>
+      <Title>Newsletter</Title>
+      <Description>Get timely updates from your favorite products.</Description>
+      <InputContainer>
+        <Input placeholder="Your email" />
+        <Button>
+          <Send />
+        </Button>
+      </InputContainer>
+    </Container>
+  );
+};
+
+export default Newsletter;
