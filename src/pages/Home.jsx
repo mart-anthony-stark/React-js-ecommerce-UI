394dbb40f7e147434e7718da2e99f62164ceea94
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-14 11:07:31 +0800

    Added component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
 
 export default function Home() {
   return (
     <div>
       <Announcement />
       <Navbar />
-      Homepage
+      <Slider />
     </div>
   );
 }
