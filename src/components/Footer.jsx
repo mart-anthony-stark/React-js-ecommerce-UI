a1f19a5d475b860b56a84ef0158de763aaf1972b
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-14 22:01:21 +0800

    Added contacts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
 import styled from "styled-components";
-import { Facebook, Twitter, Instagram, Pinterest } from "@material-ui/icons";
+import {
+  Facebook,
+  Twitter,
+  Instagram,
+  Pinterest,
+  Room,
+  Phone,
+  Mail,
+} from "@material-ui/icons";
 
 const Container = styled.div`
   display: flex;
@@ -12,15 +20,58 @@ const Left = styled.div`
 `;
 
 const Logo = styled.h1``;
-const Desc = styled.p``;
-const SocialContainer = styled.div``;
-const SocialIcon = styled.div``;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background: #${(props) => props.color};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+`;
 
 const Center = styled.div`
   flex: 1;
+  padding: 20px;
 `;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
 `;
 
 const Footer = () => {
@@ -34,20 +85,49 @@ const Footer = () => {
           tenetur quibusdam veniam nemo hic.
         </Desc>
         <SocialContainer>
-          <SocialIcon>
+          <SocialIcon color="3B5999">
             <Facebook />
           </SocialIcon>
-          <SocialIcon>
-            <Twitter />
-          </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E4405F">
             <Instagram />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
             <Pinterest />
           </SocialIcon>
         </SocialContainer>
       </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room />
+          4600,Camarines Norte, Bicol, PH
+        </ContactItem>
+        <ContactItem>
+          <Phone />
+          09994321568
+        </ContactItem>
+        <ContactItem>
+          <Mail />
+          mart@gmail.com
+        </ContactItem>
+      </Right>
     </Container>
   );
 };
