2c6292b3ec75d1efce3b46acb371bacefd90c081
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-14 15:56:59 +0800

    Added slider items data

---
+++ src/data.js
@@ -0,0 +1,25 @@
+import img1 from "./assets/fashion-girl1.png";
+import img2 from "./assets/fashion-girl.jpg";
+export const sliderItems = [
+  {
+    id: 1,
+    img: img1,
+    title: "SUMMER SALE",
+    desc: "Don't compromise on style! Get flat 30% off for new arrivals.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: img2,
+    title: "WINTER SALE",
+    desc: "Don't compromise on style! Get flat 30% off for new arrivals.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: img1,
+    title: "POPULAR SALE",
+    desc: "Don't compromise on style! Get flat 30% off for new arrivals.",
+    bg: "f5fafd",
+  },
+];
