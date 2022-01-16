50d49a6005851b0d94a8312f7ddc15b113f9b297
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-16 20:24:21 +0800

    Added login component

---
+++ src/pages/Login.jsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("https://previews.123rf.com/images/maximleshkovich/maximleshkovich1905/maximleshkovich190500088/125134566-minimal-fashion-concept-female-clothes-on-hanger-on-white-background-fashion-blog-website-social-med.jpg")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+`;
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Title = styled.div`
+  font-size: 24px;
+  font-weight: 300;
+`;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0;
+  padding: 10px;
+`;
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+          <Button>LOGIN</Button>
+          <Link>Forgot Password?</Link>
+          <Link>CREATE NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
