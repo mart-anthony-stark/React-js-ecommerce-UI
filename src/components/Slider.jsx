b930205b72f171262f70b92f4ba71e3a3925304c
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-17 20:33:03 +0800

    Added media queries

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,6 +3,7 @@ import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 import styled from "styled-components";
 import { sliderItems } from "../data";
 import { AnimatePresence, motion } from "framer-motion/dist/framer-motion";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   width: 100%;
@@ -10,6 +11,9 @@ const Container = styled.div`
   display: flex;
   position: relative;
   overflow: hidden;
+  ${mobile({
+    display: "none",
+  })}
 `;
 
 const Arrow = styled.div`
