8bfdc673813bde0dcd9fc092dae9ae2d77c5b98c
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-17 20:33:18 +0800

    Added media queries

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,9 +4,12 @@ import styled from "styled-components";
 import Products from "../components/Products";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
-const Title = styled.h1``;
+const Title = styled.h1`
+  padding: 20px;
+`;
 
 const FilterContainer = styled.div`
   display: flex;
@@ -16,17 +19,28 @@ const FilterContainer = styled.div`
 
 const Filter = styled.div`
   margin: 20px;
+  ${mobile({
+    margin: "0 10px",
+    display: "flex",
+    flexDirection: "column",
+  })}
 `;
 
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 10px;
+  ${mobile({
+    marginRight: "0px",
+  })}
 `;
 
 const Select = styled.select`
   padding: 10px;
   margin-right: 10px;
+  ${mobile({
+    margin: "10px 0px",
+  })}
 `;
 const Option = styled.option``;
 
