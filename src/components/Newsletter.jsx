80e7f241909d48efc4a4d1b8c1e90dc40c65ca1e
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-17 20:33:00 +0800

    Added media queries

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,5 +1,6 @@
 import { Send } from "@material-ui/icons";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60vh;
@@ -19,6 +20,9 @@ const Description = styled.div`
   font-size: 24px;
   font-weight: 300;
   margin-bottom: 20px;
+  ${mobile({
+    textAlign: "center",
+  })}
 `;
 
 const InputContainer = styled.div`
@@ -28,6 +32,9 @@ const InputContainer = styled.div`
   display: flex;
   justify-content: space-between;
   border: 1px solid lightgrey;
+  ${mobile({
+    width: "80%",
+  })}
 `;
 
 const Input = styled.input`
