45439b29efd570012a68158aa5c736cf88a47ff9
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-19 19:04:59 +0800

    Added link to product with category

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { mobile } from "../responsive";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   position: relative;
@@ -42,11 +43,13 @@ const Button = styled.button`
 const CategoryItem = ({ item }) => {
   return (
     <Container>
-      <Image src={item.img} />
-      <Info>
-        <Title>{item.title}</Title>
-        <Button>SHOP NOW</Button>
-      </Info>
+      <Link to={`/products/${item.cat}`}>
+        <Image src={item.img} />
+        <Info>
+          <Title>{item.title}</Title>
+          <Button>SHOP NOW</Button>
+        </Info>
+      </Link>
     </Container>
   );
 };
