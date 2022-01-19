e05828dfdb274801c8f9ef570cf17d4b93a801fe
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-19 19:57:46 +0800

    Added product filter

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { popularProducts } from "../data";
 import Product from "./Product";
+import { useState, useEffect } from "react";
 
 const Container = styled.div`
   padding: 20px;
@@ -9,7 +10,40 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
-const Products = () => {
+const Products = ({ cat, sort, filters }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await fetch(
+          cat
+            ? `http://localhost:5000/api/products?category=${cat}`
+            : "http://localhost:5000/api/products"
+        );
+        const data = await res.json();
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProducts();
+  }, [cat]);
+
+  useEffect(() => {
+    () => {
+      cat &&
+        setFilteredProducts(
+          products.filter((item) =>
+            Object.entries(filters).every(([key, value]) => {
+              item[key].includes(value);
+            })
+          )
+        );
+    };
+  }, [products, cat, filters]);
+
   return (
     <Container>
       {popularProducts.map((product) => (
