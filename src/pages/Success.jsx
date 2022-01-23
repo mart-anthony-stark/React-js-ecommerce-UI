fdaa33698f8fee125d1f77fa58529fd2827b0020
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-23 19:36:39 +0800

    Added success page

---
+++ src/pages/Success.jsx
@@ -0,0 +1,10 @@
+import { useLocation } from "react-router-dom";
+
+const Success = () => {
+  const location = useLocation();
+
+  console.log(location);
+  return <div>successful</div>;
+};
+
+export default Success;
