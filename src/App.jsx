1f807c202b374c9711b4914111939b71ae10673c
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-23 21:14:51 +0800

    Added route guards

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,30 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const App = () => {
-  const user = true;
+  const user = useSelector((state) => state.user.currentUser);
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products/:category" element={<ProductList />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/success" element={<Success />} />
+        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+        <Route
+          path="/products/:category"
+          element={user ? <ProductList /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/product/:id"
+          element={user ? <Product /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/cart"
+          element={user ? <Cart /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/success"
+          element={user ? <Success /> : <Navigate to="/login" />}
+        />
         <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
         <Route
           path="/register"
